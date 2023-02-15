9234ee28d6e5632e2fd780e1114a8a1fa73d6fa4
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-15 09:55:45 +0000

    update mongodb connection to allow it to work globally

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,28 @@
-import express from "express";   // Importing Express library
+import express from "express"; // Importing Express library
 import mongoose from "mongoose"; // Importing Mongoose library
-import cors from "cors";         // Importing Cors library
+import cors from "cors"; // Importing Cors library
 import UserRoute from "./routes/UserRoute.js"; // Importing UserRoute
 
 const app = express(); // Creating an instance of the Express application
 
-mongoose.connect('mongodb://localhost:27017/assetinsightproject',{ // Connecting to the MongoDB database
-    useNewUrlParser: true,     // Set the useNewUrlParser flag to true
-    useUnifiedTopology: true   // Set the useUnifiedTopology flag to true
-});
+// Connecting to the MongoDB database
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.stzluip.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true, // Set the useNewUrlParser flag to true
+      useUnifiedTopology: true, // Set the useUnifiedTopology flag to true
+    }
+  )
+  .then(() => console.log("Connected to database ..."))
+  .catch((error) => console.log(error));
 
 const db = mongoose.connection; // Getting the default connection of Mongoose
-db.on('error', (error) => console.log(error)); // Log error if any while connecting to the database
-db.once('open', () => console.log('Connected to database ...')); // Log successful connection to the database
+db.on("error", (error) => console.log(error)); // Log error if any while connecting to the database
+db.once("open", () => console.log("Connected to database ...")); // Log successful connection to the database
 
-app.use(cors());         // Using Cors middleware to allow cross-origin resource sharing
+app.use(cors()); // Using Cors middleware to allow cross-origin resource sharing
 app.use(express.json()); // Using the express.json() middleware to parse incoming JSON data
-app.use(UserRoute);      // Mounting the UserRoute on the app
+app.use(UserRoute); // Mounting the UserRoute on the app
 
 app.listen(5000, () => console.log("Server running...")); // Starting the server on port 5000 and logging a message to the console when the server starts
