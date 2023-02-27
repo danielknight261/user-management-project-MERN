7c420c6ddf92a3feb9128166273a18f3acbdb048
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-27 14:19:33 +0000

    small camel case fix , there was combination of camel and snake

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,49 +3,52 @@ import  User  from "../models/UserModel.js";
 // Get all users
 export const getUsers = async (req, res) => {
     try {
-        const users = await User.find();
-        res.json(users);
+        const allUsers = await User.find();
+        res.status(200).json(allUsers);
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: "server error" });
     }
-}
+};
 
 // Get user by ID
 export const getUserById = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        res.json(user);
+        const requestedUser = await User.findById(req.params.id);
+        res.json(requestedUser);
     } catch (error) {
-        res.status(404).json({message: error.message});
+        res.status(404).json({ message: "user not found" });
     }
-}
+};
 
 // Save a new user
 export const saveUser = async (req, res) => {
-    const user = new User(req.body);
+    const newUser = new User(req.body);
     try {
-        const insertuser = await user.save();
-        res.status(201).json(insertuser);
+        const insertedUser = await newUser.save();
+        res.status(201).json(insertedUser);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
     }
-}
+};
 
 // Update a user by ID
 export const updateUser = async (req, res) => {
     try {
-        const updateuser = await User.updateOne({_id:req.params.id}, {$set:req.body});
-        res.status(200).json(updateuser);
+        const updatedUser = await User.updateOne(
+            { _id: req.params.id },
+            { $set: req.body }
+        );
+        res.status(200).json(updatedUser);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: "user not found" });
     }
-}
+};
 
 // Delete a user by ID
 export const deleteUser = async (req, res) => {
     try {
-        const deleteuser = await User.deleteOne({_id:req.params.id});
-        res.status(200).json(deleteuser);
+        const deleteUser = await User.deleteOne({_id:req.params.id});
+        res.status(200).json(deleteUser);
     } catch (error) {
         res.status(400).json({message: error.message});
     }
